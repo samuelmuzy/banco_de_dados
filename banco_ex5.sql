9f3b4191a7f8362ba54a2c75ee7ec2919d75f333
samuel muzy fernandes <135380750+samuelmuzy@users.noreply.github.com>
2024-08-28 12:56:48 -0300

    Update banco_ex5.sql

--- banco_ex5.sql
+++ banco_ex5.sql
@@ -53,4 +53,79 @@ UPDATE alunos_diciplina SET P1 = 17.0,P2 = 14.5,FALTAS = 10 WHERE id_aluno = 5 a
 
 UPDATE alunos_diciplina SET P1 = 38.1,P2 = 10.5,FALTAS = 7 WHERE id_aluno = 5 and id_diciplina = 8;
 
+UPDATE alunos_diciplina SET P1 = 49.0,P2 = 35.5,FALTAS = 14 WHERE id_aluno = 6 and id_diciplina = 2;
 
+UPDATE alunos_diciplina SET P1 = 18.8,P2 = 25.7,FALTAS = 2 WHERE id_aluno = 6 and id_diciplina = 3;
+
+UPDATE alunos_diciplina SET P1 = 29.1,P2 = 33.7,FALTAS = 5 WHERE id_aluno = 6 and id_diciplina = 7;
+
+UPDATE alunos_diciplina SET P1 = 37.6,P2 = 22.4,FALTAS = 3 WHERE id_aluno = 6 and id_diciplina = 8;
+
+
+UPDATE alunos_diciplina SET P1 = 17.9,P2 = 44.5,FALTAS = 7 WHERE id_aluno = 7 and id_diciplina = 2;
+
+UPDATE alunos_diciplina SET P1 = 12.0,P2 = 32.4,FALTAS = 0 WHERE id_aluno = 7 and id_diciplina = 4;
+
+UPDATE alunos_diciplina SET P1 = 9.2,P2 = 27.3,FALTAS = 11 WHERE id_aluno = 7 and id_diciplina = 7;
+
+UPDATE alunos_diciplina SET P1 = 28.0,P2 = 36.1,FALTAS = 4 WHERE id_aluno = 7 and id_diciplina = 8;
+
+
+
+UPDATE alunos_diciplina SET P1 = 13.0,P2 = 26.7,FALTAS = 10 WHERE id_aluno = 8 and id_diciplina = 1;
+
+UPDATE alunos_diciplina SET P1 = 29.4,P2 = 34.1,FALTAS = 1 WHERE id_aluno = 8 and id_diciplina = 2;
+
+UPDATE alunos_diciplina SET P1 = 45.5,P2 = 16.8,FALTAS = 12 WHERE id_aluno = 8 and id_diciplina = 3;
+
+UPDATE alunos_diciplina SET P1 = 38.7,P2 = 11.2,FALTAS = 2 WHERE id_aluno = 8 and id_diciplina = 4;
+
+UPDATE alunos_diciplina SET P1 = 4.0,P2 = 18.5,FALTAS = 12 WHERE id_aluno = 8 and id_diciplina = 5;
+
+UPDATE alunos_diciplina SET P1 = 21.6,P2 = 40.8,FALTAS = 6 WHERE id_aluno = 8 and id_diciplina = 6;
+
+UPDATE alunos_diciplina SET P1 = 37.6,P2 = 12.7,FALTAS = 8 WHERE id_aluno = 8 and id_diciplina = 7;
+
+UPDATE alunos_diciplina SET P1 = 22.1,P2 = 42.3,FALTAS = 1 WHERE id_aluno = 8 and id_diciplina = 8;
+
+UPDATE alunos_diciplina SET P1 = 49.2,P2 = 29.9,FALTAS = 0 WHERE id_aluno = 8 and id_diciplina = 9;
+
+
+
+UPDATE alunos_diciplina SET P1 = 7.0,P2 = 44.5,FALTAS = 9 WHERE id_aluno = 1 and id_diciplina = 1;
+
+UPDATE alunos_diciplina SET P1 = 41.8,P2 = 36.8,FALTAS = 14 WHERE id_aluno = 1 and id_diciplina = 2;
+
+UPDATE alunos_diciplina SET P1 = 12.6,P2 = 40.2,FALTAS = 8 WHERE id_aluno = 1 and id_diciplina = 6;
+
+UPDATE alunos_diciplina SET P1 = 26.4,P2 = 18.7,FALTAS = 11 WHERE id_aluno = 1 and id_diciplina = 7;
+
+UPDATE alunos_diciplina SET P1 = 47.4,P2 = 32.1,FALTAS = 5 WHERE id_aluno = 1 and id_diciplina = 9;
+
+
+
+UPDATE alunos_diciplina SET P1 = 0.0,P2 = 37.5,FALTAS = 12 WHERE id_aluno = 2 and id_diciplina = 3;
+
+UPDATE alunos_diciplina SET P1 = 48.5,P2 = 28.9,FALTAS = 4 WHERE id_aluno = 2 and id_diciplina = 6;
+
+UPDATE alunos_diciplina SET P1 = 37.3,P2 = 39.1,FALTAS = 13 WHERE id_aluno = 2 and id_diciplina = 8;
+
+
+
+UPDATE alunos_diciplina SET P1 = 29.4,P2 = 50.0,FALTAS = 3 WHERE id_aluno = 3 and id_diciplina = 3;
+
+UPDATE alunos_diciplina SET P1 = 14.0,P2 = 4.5,FALTAS = 2 WHERE id_aluno = 3 and id_diciplina = 4;
+
+UPDATE alunos_diciplina SET P1 = 25.2,P2 = 33.8,FALTAS = 1 WHERE id_aluno = 3 and id_diciplina = 8;
+
+
+
+UPDATE alunos_diciplina SET P1 = 27.8,P2 = 42.1,FALTAS = 0 WHERE id_aluno = 4 and id_diciplina = 1;
+
+UPDATE alunos_diciplina SET P1 = 45.3,P2 = 21.2,FALTAS = 11 WHERE id_aluno = 4 and id_diciplina = 4;
+
+UPDATE alunos_diciplina SET P1 = 3.7,P2 = 36.3,FALTAS = 15 WHERE id_aluno = 4 and id_diciplina = 5;
+
+UPDATE alunos_diciplina SET P1 = 16.5,P2 = 41.7,FALTAS = 5 WHERE id_aluno = 4 and id_diciplina = 7;
+
+UPDATE alunos_diciplina SET P1 = 46.2,P2 = 44.5,FALTAS = 2 WHERE id_aluno = 4 and id_diciplina = 8;
